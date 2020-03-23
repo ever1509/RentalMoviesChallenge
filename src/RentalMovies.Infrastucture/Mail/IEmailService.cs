7b9e20ac629faf8c2e4b2b523ae606112d07e92c
ever1509 <ever1509@gmail.com>
2020-03-23 10:41:38 -0600

    Adding RentalMovies.Infrastucture in order to handle mail configuration

---
+++ src/RentalMovies.Infrastucture/Mail/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RentalMovies.Infrastucture.Mail.EmailConfiguration;
+
+namespace RentalMovies.Infrastucture.Mail
+{
+    public interface IEmailService
+    {
+        void Send(EmailMessage emailMessage);
+        List<EmailMessage> ReceiveEmail(int maxCount = 10);
+
+    }
+}
